ef21d1620a5b07a99698ba6d4f6e294e1e8378db
shaozhe.ysz <shaozhe.ysz@alibabapictures.com>
2018-06-14 17:59:44 +0800

    增加@types标识

---
+++ build/index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * @fileOverview
+ * -------------------------
+ * @author iNahoo
+ * @since 2018/6/14.
+ */
+
+export class VO {
+    static create(Scheme): typeof VO ;
+    static transform(Fn): typeof VO ;
+}
