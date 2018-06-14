5daeb86160f7e9a7bab7c74362b7485452bf1afc
shaozhe.ysz <shaozhe.ysz@alibabapictures.com>
2018-06-14 17:46:27 +0800

    some test

--- node-test/index.js
+++ node-test/index.js
@@ -5,8 +5,39 @@
  */
 "use strict";
 
-const what = require("../build/index");
+const {VO} = require("../build/index");
 
-const VO = what.VO;
+const M = VO.create({
+    a: Number,
+    b: String,
+    c: [Number],
+    d: {
+        d1: Number,
+        d2: String,
+    }
+});
 
-console.log(what);
+const mock = {
+    a: '123',
+    b: '123',
+    c: ['123', 123, 234],
+    d: {
+        d1: '123',
+        d3: '123',
+    },
+    e: 123,
+};
+
+
+const M2 = M.transform(obj => {
+    return {
+        ...obj,
+        x: obj.a + obj.d.d1,
+    }
+});
+
+console.log(new M(mock));
+console.log(new M2(mock));
+
+
+
